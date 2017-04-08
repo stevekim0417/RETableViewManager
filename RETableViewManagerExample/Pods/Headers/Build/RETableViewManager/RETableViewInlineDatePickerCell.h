43a0a46407f6066e7f2ce072559298076682e94c
Steve Kim <steve.kim.0417@gmail.com>
2017-04-09 01:15:55 +0900

    Append Example

--- RETableViewManagerExample/Pods/Headers/Build/RETableViewManager/RETableViewInlineDatePickerCell.h
+++ RETableViewManagerExample/Pods/Headers/Build/RETableViewManager/RETableViewInlineDatePickerCell.h
@@ -1,1 +1,16 @@
-../../../../../RETableViewManager/Cells/RETableViewInlineDatePickerCell.h
+//
+//  RETableViewInlineDatePickerCell.h
+//  RETableViewManagerExample
+//
+//  Created by Roman Efimov on 10/4/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import "RETableViewCell.h"
+#import "REInlineDatePickerItem.h"
+
+@interface RETableViewInlineDatePickerCell : RETableViewCell
+
+@property (strong, readwrite, nonatomic) REInlineDatePickerItem *item;
+
+@end
